a448f6b139a5fcaf1e8d647237b3a167e1a1e49d
Bosnyák Imre Dávid <suti211@gmail.com>
2016-10-27 12:28:50 +0200

    Fixed typos, comments

--- hello.py
+++ hello.py
@@ -1,11 +1,12 @@
 import sys
 
+
 # this function returns a string, based on arguments given in command line
 # if no arguments given it returns a "world" string
 # with 1 argument, return the argument itself
 # with 2 arguments returns the the two args in 1 string
 # with 3 arguments or more we assume the user is bullshitting, and return the firs arg.
-# if the arguments first lettel is lowercase the code will try to switch it to uppercase with the .title() function
+# if the arguments first letter is lowercase the code will try to switch it to uppercase with the .title() function
 def sGetNameFromArgs():
     if(len(sys.argv) == 1):
         sPersonName = "World"
@@ -15,9 +16,9 @@ def sGetNameFromArgs():
         sPersonName = sys.argv[1].title() + " " + sys.argv[2].title()
     return sPersonName
 
-# takes a string as parameter, so we can modify the welcome word
-# returns a message string with the welcoming word, and the name given by the User 
-# with the help of the sGetNameFromArgs() function
+
+# takes two strings as parameter, 1 word used to welcome,
+# and 1 name to welcome, and returns these as a string.
 def sConstructMessage(sWelcomeWord, sUserName):
     sMessage = sWelcomeWord + " " + sUserName + "!"
     return sMessage
